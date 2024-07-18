874a27f7037483e97b987cbe49a327acae4e43de
Matheus Vinicius <matheusv.16santos@gmail.com>
2024-07-18 20:29:13 -0300

    refactor: Modificacao na classe de excessao da conexao com o banco

--- src/Exceptions/DBConnectionInvalid.php
+++ src/Exceptions/DBConnectionInvalid.php
@@ -3,21 +3,16 @@
 namespace Mviniis\ConnectionDatabase\Exceptions;
 
 use \Exception;
-use \Illuminate\Http\JsonResponse;
 
 /**
  * class DBConnectionInvalid
  * 
- * Classe responsável por exibir uma exception de erro ao conectar ou executar uma opração no banco de dados
+ * Classe responsável por exibir uma exception de erro ao conectar ou executar uma operação no banco de dados
  * 
  * @author Matheus Vinicius <matheusv.16santos@gmail.com>
  */
 class DBConnectionInvalid extends Exception {
-  public function render($request) {
-    $requestCode = $this->getCode();
-    return response()->json(
-      ['error' => $this->getMessage()], 
-      ($requestCode > 0) ? $requestCode: JsonResponse::HTTP_INTERNAL_SERVER_ERROR
-    );
+  public function __construct(string $message, int $code = 0) {
+    parent::__construct($message, $code);
   }
 }
